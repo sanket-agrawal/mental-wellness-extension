2da70c4bd808ac871d7143430690f5286740f67f
Sanket <mailsanketagrawal@gmail.com>
2026-06-23 19:51:12 +0530

    [fix] : worker registration issue

--- background.ts
+++
@@ -1,9 +0,0 @@
-import type { PlasmoMessaging } from "@plasmohq/messaging"
-
-const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  res.send({
-    message: "Hello from background"
-  })
-}
-
-export default handler
